f3380721cad24ad4f4680e91fa9de0fe3c8a71d1
Jordan Leppert <jordanleppert@gmail.com>
2021-06-13 18:46:12 +0100

    Various changes and fixes

--- chunk_trimmer.py
+++ chunk_trimmer.py
@@ -8,10 +8,12 @@ import sys
 
 scriptpath = sys.argv.pop(0)
 
+# Default argument values
 region_file = None
-cutoff_ticks = 1200
+cutoff_ticks = 1200 # 60 seconds (20 ticks per second * 60)
 dry_run = False
 
+# Get arguments
 for a in sys.argv:
     i = a.index('=')
     k = a[0 : i]
@@ -28,21 +30,27 @@ for a in sys.argv:
     else:
         fatal("No such argument: '{}'".format(k))
 
+# Get list of files to trim
 files = []
 if region_file is not None:
     files = [region_file]
 else:
     files = os.listdir()
 
+# Delete all chunks in files less inhabited than the cutoff number of ticks
 for file in files:
     region = RegionFile(file)
     chunks = region.get_chunks()
     for chunk in chunks:
         x = chunk['x']
         z = chunk['z']
-        chunk_nbt = region.get_chunk(x, z)
+        try:
+            chunk_nbt = region.get_chunk(x, z)
+        except Exception as e:
+            print("Error with file {} chunk {},{}".format(file, x, z), file=sys.stderr)
+            raise e
         inhabited_time = chunk_nbt['Level']['InhabitedTime'].value
         if inhabited_time < cutoff_ticks:
             if not dry_run:
                 region.unlink_chunk(x, z)
-            print("{},{},{},{}:".format(file, x, z, inhabited_time))
+            print("{},{},{},{}".format(file, x, z, inhabited_time))
